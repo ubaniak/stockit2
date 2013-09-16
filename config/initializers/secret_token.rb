33da85e792546bbadab8d778bfd915810a4928fb
Ubaniak <bhavek.budhia@gmail.com>
2013-09-15 19:16:40 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockit2::Application.config.secret_key_base = '464e036e4476aa8eb9e8734bf58237c54ec60b978d2a5dbf8190e1cdad7519f6bc75d1a308b037835f9dd596f9bd04c0192e54120fc65880ca88fc0181d8a565'
