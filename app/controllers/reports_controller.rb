52b07517936aae3218b9b19a032adc85b6066ab1
Ubaniak <bhavek.budhia@gmail.com>
2013-09-19 18:13:27 -0500

    finished daily report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,8 +7,11 @@ class ReportsController < ApplicationController
 
     def daily_report
         @date = params[:date] ? Date.parse(params["date"]) : Date.today
+
         @sales = Sale.find_all_by_date(@date)
         @refunds = Refund.find_all_by_date(@date)
         @invoices = Invoice.find_all_by_date(@date)
+        @bills = Bill.find_all_by_date(@date)
+        @account_transfers = AccountTransfer.find_all_by_date(@date)
     end
 end
