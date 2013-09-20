c6d0ddfcccb16e4068a72a42a3916d1de31e6701
Ubaniak <bhavek.budhia@gmail.com>
2013-09-20 07:05:58 -0500

    remove_from_sale is now a link

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -22,30 +22,15 @@ class SalesController < ApplicationController
     end
 
     def add_to_sale
-        stock = Stock.find(params[:id])
-        transaction = SaleTransaction.find_by_stock_id_and_user_id(stock.id, current_user.id)
-        if transaction.nil?
-            transaction = SaleTransaction.new
-            transaction.user_id = current_user.id
-            transaction.stock = stock
-        else
-            transaction.qty += 1
-        end
-        transaction.save
-
+        SaleTransaction.add_to_sale(params[:id], current_user.id)
         @current_sales = SaleTransaction.find_all_by_user_id(current_user.id)
     end
 
     def remove_from_sale
-        transaction = SaleTransaction.find(params[:id])
-        transaction.qty -= 1
-        if transaction.qty <= 0
-            transaction.destroy
-        else
-            transaction.save
-        end
+        SaleTransaction.remove_from_sale(params[:id])
 
         @current_sales = SaleTransaction.find_all_by_user_id(current_user.id)
+        redirect_to sales_landing_path
     end
 
     def landing
--- app/models/sale_transaction.rb
+++ app/models/sale_transaction.rb
@@ -4,11 +4,32 @@ class SaleTransaction < ActiveRecord::Base
     belongs_to :user
 
     before_save :set_defaults
-    before_destroy :remove_sales
 
     def set_defaults
         self.qty ||= 1
         self.sell_price ||= self.stock.sell_price
+    end
+
+    def self.add_to_sale(stock_id, user_id)
+        transaction = SaleTransaction.find_by_stock_id_and_user_id(stock_id, user_id)
+        if transaction.nil?
+            transaction = self.new
+            transaction.user_id = user_id
+            transaction.stock_id = stock_id
+        else
+            transaction.qty += 1
+        end
+        transaction.save
+    end
+
+    def self.remove_from_sale(id)
+        transaction = SaleTransaction.find(id)
+        transaction.qty -= 1
+        if transaction.qty <= 0
+            transaction.destroy
+        else
+            transaction.save
+        end
     end
 
     def remove_sales
